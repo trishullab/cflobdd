3927f1e465b1602bfaa213c8b9d4ecd0be522e6a
Meghana <sistla.meghana@gmail.com>
2023-02-03 01:16:04 -0600

    Minor change

--- CFLOBDD/vector_complex_float_boost_top_node.cpp
+++ CFLOBDD/vector_complex_float_boost_top_node.cpp
@@ -165,7 +165,7 @@ namespace CFL_OBDD {
 				if (n->rootConnection.returnMapHandle.Lookup(i) != 0){
 					long double v = n->rootConnection.returnMapHandle.Lookup(i).real().convert_to<long double>();
 					long double logNumPaths = n->rootConnection.entryPointHandle->handleContents->numPathsToExit[i];
-					prob += v * std::powl(2, logNumPaths);
+					prob += v * std::pow(2, logNumPaths);
 				}
 			}
 			return prob;
@@ -180,7 +180,7 @@ namespace CFL_OBDD {
 					if (abs(v - p) < DBL_MIN)
 					{
 						long double logNumPaths = n->rootConnection.entryPointHandle->handleContents->numPathsToExit[i];
-						return std::powl(2, logNumPaths);
+						return std::pow(2, logNumPaths);
 					}
 				}
 			}
